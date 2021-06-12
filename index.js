c852a48aa33e9e1f61f11e734fe5f8db75c732ab
Justin Hui <huirayj@gmail.com>
2021-06-12 09:06:31 -0400

    added comments

--- index.js
+++ index.js
@@ -10,13 +10,16 @@ const question = require('./lib/questions');
 const render = require("./lib/htmlRenderer");
 const teams = require('./lib/teams');
 
+// starts application
 const init = () => {
     createTeam();
 }
 
 const createTeam = () => {
+    // present questions to user
     inquirer.prompt(question.questionTeam)
         .then((answer) => {
+            // team name is used as a property of the team object
             teams[`${answer.teamName}`] = [];
             addManager();
         });
@@ -27,6 +30,7 @@ const addManager = () => {
         .then(({ name, id, email, office }) => {
             const manager = new Manager(name, id, email, office);
 
+            // adds manager to employee array
             teams[Object.keys(teams)].push(manager);
             addMember();
         });
@@ -37,6 +41,7 @@ const addEngineer = () => {
         .then(({ name, id, email, github }) => {
             const engineer = new Engineer(name, id, email, github);
 
+            // adds engineer to employee array
             teams[Object.keys(teams)].push(engineer);
             addMember();
         });
@@ -47,6 +52,7 @@ const addIntern = () => {
         .then(({ name, id, email, school }) => {
             const intern = new Intern(name, id, email, school);
 
+            // adds intern to employee array
             teams[Object.keys(teams)].push(intern);
             addMember();
         });
@@ -70,10 +76,12 @@ const addMember = () => {
 }
 
 const generateHTML = () => {
+    // formats team name for filename consistency
     const filename = Object.keys(teams).toString().replace(/\s+/g, '').toLowerCase();
     const OUTPUT_DIR = path.resolve(__dirname, "dist");
     const outputPath = path.join(OUTPUT_DIR, `${filename}.html`);
 
+    // takes the employee array and processes it as an html document
     fs.writeFileSync(outputPath, render(teams[Object.keys(teams)]), (err) =>
         console.error(err ? err : 'Your html was successfully created.'));
 };
